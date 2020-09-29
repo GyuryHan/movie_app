6c05e21d08671ae5b8c249df47b0fe449e9173be
GyuryHan <pos7271@daum.net>
2020-09-29 21:18:45 +0900

    Updates

---
+++ precache-manifest.38eb905aacb87be5e7b7979315c29542.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b38bfefdbcf6df4ec1aa668db764926d",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "591dc4d85fcee4d93f2e",
+    "url": "/movie_app/static/css/main.4268e578.chunk.css"
+  },
+  {
+    "revision": "7b96c93ef25ce9312641",
+    "url": "/movie_app/static/js/2.8995e99f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.8995e99f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "591dc4d85fcee4d93f2e",
+    "url": "/movie_app/static/js/main.14f639ba.chunk.js"
+  },
+  {
+    "revision": "62c304eb128ebf65007f",
+    "url": "/movie_app/static/js/runtime-main.909b7464.js"
+  }
+]);
